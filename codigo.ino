066fd14693366717918788f2e055e4d85a08da53
santiagocastano-max <52548153+santiagocastano-max@users.noreply.github.com>
2020-02-15 11:05:20 -0500

    Update codigo.ino

--- codigo.ino
+++ codigo.ino
@@ -1,28 +1,14 @@
-const int ledPIN = 9;
+const int ledPin = 13;
 
-int option;
 
 void setup(){
   Serial.begin(9600);
-  pinMode(ledPIN , OUTPUT); 
+  pinMode(ledPin , OUTPUT); 
 }
 
 void loop(){
-  //si existe información pendiente
-  if (Serial.available()>0){
-    //leeemos la opcion
-    char option = Serial.read();
-    //si la opcion esta entre '1' y '9'
-    if (option >= '1' && option <= '9')
-    {
-      //restamos el valor '0' para obtener el numero enviado
-      option -= '0';
-      for(int i=0;i<option;i++){
-         digitalWrite(ledPIN , HIGH);
-         delay(100);
-         digitalWrite(ledPIN , LOW);
-         delay(200);
-      }
-    }
-  }
+ digitalWrite(ledPin,HIGH);
+  delay(1000);
+  digital Write(1000);
+  delay(1000);
 }
